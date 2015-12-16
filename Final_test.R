3ffde52ef2599f10683424fc863f76d957b0be93
Rohan <rohanfray@gmail.com>
2015-12-16 04:47:30 +0000

    commit

--- Final_test.R
+++ Final_test.R
@@ -2,3 +2,21 @@ conn <- file("/home/tengig/tmpNFvucr",open="r")
 linn <-readLines(conn)
 s<-linn[233313]
 fromJSON(s)
+
+j=0
+x <- numeric(330071)
+y <- character(330071)
+y2 <- character(330071)
+for (i in 1:length(linn)){
+  tmp = fromJSON(linn[i])
+  if (tmp$type == "review"){
+    x[j] <- tmp$stars
+    y[j] <- tmp$user_id
+    y2[j] <- tmp$business_id
+    j=j+1
+  }
+  
+}
+
+df <- data.frame(y,y2,x, stringsAsFactors=FALSE)
+colnames(df) <- c("userID","businessID","rating")
