aa8f31e68d2e2eac99145186ebe4c304fa04a87d
Rohan <rohanfray@gmail.com>
2015-12-20 18:23:58 +0000

    commit

--- Irlba.R
+++ Irlba.R
@@ -1,6 +1,17 @@
 library(Matrix)
+library(irlba)
+
+
+us<-as.factor(df.scale$users)
+us.int <-as.integer(us)
+
+bs<-as.factor(df.scale$businesses)
+bs.int <-as.integer(bs)
+
 mat<-with(df.scale, 
-          sparseMatrix(i =as.numeric(users), 
-                       j=as.numeric(businesses), 
+          sparseMatrix(i=us.int, 
+                       j=bs.int, 
                        x=ratings,
-                       dimnames=list(levels(users), levels(businesses))))
+                       dimnames=list(levels(us), levels(bs))))
+
+L <- irlba(mat,nv = 2,nu = 2)
