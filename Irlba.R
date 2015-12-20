c3f3e30bbbbe371b2546d31df1b4c1a57f38013b
Rohan <rohanfray@gmail.com>
2015-12-20 22:42:08 +0000

    commit

--- Irlba.R
+++ Irlba.R
@@ -14,4 +14,30 @@ mat<-with(df.scale,
                        x=ratings,
                        dimnames=list(levels(us), levels(bs))))
 
+
 L <- irlba(mat,nv = 2,nu = 2)
+
+L.d <- diag(L$d)
+rev<-L$u%*%L.d
+rownames(rev)<-levels(us)
+
+rev.v <- t(L$v)
+colnames(rev.v)<-levels(bs)
+
+rm(list = c("us","us.int","bs","bs.int"))
+
+
+Uvals<- matrix(unlist(lapply(testdf2$users, function(x){rev[x,]})),ncol=d,byrow = T)
+Vvals<- t(matrix(unlist(lapply(testdf2$businesses,function(x){rev.v[,x]})),nrow=d,byrow=F))
+
+vals<-apply(cbind(Uvals,Vvals),1,function(x){
+  newcalc<-0
+  for(i in range(1:d)){
+    newcalc = newcalc + x[i]*x[d+i]
+  }
+  newcalc
+})
+SE <- (vals-testdf$ratings)^2
+MSE <- sum(SE)/nrow(df.scale)
+RMSE.irlba <- sqrt(MSE)
+RMSE.irlba
